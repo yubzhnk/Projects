db8a0b281639e99fecfba9af46cc9b9845a1e2f2
yubzhnk <21fi.yu.bozhenko@std.npu.edu.ua>
2022-12-22 13:35:50 +0200

    Lab3

---
+++ Lab3_Calculator/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        float a, b;
+        int count;
+        bool znak = true;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress_1);
+           
+        }
+     
+               Point lastPoint;
+
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+
+
+            }
+        }
+
+      
+        private void textBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8 && number != 44 && number != 45)
+            {
+                e.Handled = true;
+                MessageBox.Show("Поле повинне містити тільки цифри");
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void buttonC_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            label1.Text = "";
+        }
+
+        private void button_clear_one_Click(object sender, EventArgs e)
+        {
+            int lenght = textBox1.Text.Length - 1;
+            string text = textBox1.Text;
+            textBox1.Clear();
+            for (int i = 0; i < lenght; i++)
+            {
+                textBox1.Text = textBox1.Text + text[i];
+            }
+        }
+
+        private void button_plus_minus_Click(object sender, EventArgs e)
+        {
+            if (znak == true)
+            {
+                textBox1.Text = "-" + textBox1.Text;
+                znak = false;
+            }
+            else if (znak == false)
+            {
+                textBox1.Text = textBox1.Text.Replace("-", "");
+                znak = true;
+
+            }
+        }
+
+        private void button_plus_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 1;
+            label1.Text = a.ToString() + "+";
+            znak = true;
+        }
+
+     
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+        }
+        private void number1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+        }
+
+        private void number2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+        }
+
+        private void number3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+        }
+        private void number4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+        }
+
+        private void number5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+        }
+
+        private void number6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+        }
+
+        private void number7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+        }
+
+        private void number8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+        }
+
+        private void number9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+        }
+
+        private void button_minus_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 2;
+            label1.Text = a.ToString() + "-";
+            znak = true;
+        }
+
+        private void button_m_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 3;
+            label1.Text = a.ToString() + "*";
+            znak = true;
+        }
+
+        private void button_d_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 4;
+            label1.Text = a.ToString() + "/";
+            znak = true;
+        }
+
+        private void button_result_Click(object sender, EventArgs e)
+        {
+            calculate();
+            label1.Text = "";
+        }
+
+        private void button_tocka_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ",";
+        }
+
+        private void calculate()
+        {
+            switch (count)
+            {
+                case 1:
+                    b = a + float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 2:
+                    b = a - float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 3:
+                    b = a * float.Parse(textBox1.Text);
+
+                    textBox1.Text = b.ToString();
+                    break;
+                case 4:
+                    b = a / float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+
+
+
+
+
+
+
+                default:
+                    if (float.Parse(textBox1.Text) == 0)
+                        b = 0;
+                    textBox1.Text = b.ToString();
+                    break;
+            }
+
+        }
+
+    }
+}
+
